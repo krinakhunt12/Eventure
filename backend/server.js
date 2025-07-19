ebf89412b2fee10dc8f573df5c93148c90b31205
Krina Khunt <krinakhunt12@gmail.com>
2025-07-19 22:22:09 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,5 @@ mongoose.connect(process.env.MONGO_URI, {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("MongoDB connected");
 }); 
